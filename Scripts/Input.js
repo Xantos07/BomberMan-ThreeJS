1356b52e5d9e8ce0f1b9dd33273c92210349e46c
Xantos07 <coalstudio7@gmail.com>
2024-02-11 10:44:47 +0100

    InputPlayer

---
+++ Scripts/Input.js
@@ -0,0 +1,30 @@
+
+const keys = {};
+
+document.addEventListener('keydown', handleKeyDown);
+document.addEventListener('keyup', handleKeyUp);
+
+function handleKeyDown(event) {
+    console.log(event)
+    keys[event.key] = true;
+}
+
+function handleKeyUp(event) {
+    keys[event.key] = false;
+}
+
+export function GetMovementDirection() {
+    if (keys['z']) {
+        return 'up';
+    } else if (keys['q']) {
+        return 'left';
+    } else if (keys['s']) {
+        return 'down';
+    } else if (keys['d']) {
+        return 'right';
+    } else if (keys[' ']) {
+        return 'placeBomb';
+    } else {
+        return 'none';
+    }
+}
