5ef0a074d33e04a061abd65caa29dff5638e7728
Xantos07 <coalstudio7@gmail.com>
2024-02-17 17:27:13 +0100

    AstarsAlgorithme

--- Scripts/PathFinding/PathFinding.js
+++ Scripts/PathFinding/PathFinding.js
@@ -1,34 +1,79 @@
-import {tiles} from "/Scripts/Grid.js";
+import {checkIsOutside, tiles} from "/Scripts/Grid.js";
 
 //Take Start Tile and Destination Tile
 //returns Tiles[,] to destination or nearest destination
 
-function Path(start, end){
+//Cost Tile
+//G => Distance to start node
+//H => Distance to ending node
+//F => G + H
+function Path(start, end) {
 
-    let paths = [,];
-    let queue = [];
-    let range = 1;
+    const startNode = start;
+    const destinationNode = end;
 
-    queue.push(start);
+    const open = [];
+    const close = [];
+
+    open.push(startNode);
+
+    while (open.length > 0) {
+
+        let current = open[0];
+
+        for (let i = 1; i < open.length; i++) {
+            if (open[i].F < current.F || open[i].F == current.F) {
+                if (open[i].H < current.H)
+                    current = open[i];
+            }
+        }
+
+        open.splice(current);
+        close.push(current);
+
+        //Return RetracePath
+
+        let neighbours = getNeighbours(current);
+
+        neighbours.forEach(neighbour => {
+            if(!neighbour.isEmpty || neighbours.includes(neighbour))
+            {
+                return;
+            }
+        });
+    }
+}
+
+function getNeighbours(tile) {
+
+    const neightboursTile = [];
 
     const directions = [
-        { dirX: 1, dirY: 0 },  // Right
-        { dirX: -1, dirY: 0 }, // Left
-        { dirX: 0, dirY: 1 },  // Up
-        { dirX: 0, dirY: -1 }  // Down
+        {dirX: 1, dirY: 0},  // Right
+        {dirX: -1, dirY: 0}, // Left
+        {dirX: 0, dirY: 1},  // Up
+        {dirX: 0, dirY: -1}  // Down
     ];
 
     for (const dir of directions) {
-        let dirX = dir.dirX + queue[0].x + range;
-        let dirY = dir.dirY + queue[0].y + range;
+        let dirX = dir.dirX;
+        let dirY = dir.dirY;
 
-        console.log(" DIRECTION TO START : ", dirX , " /// " , dirY);
+        for (let i = 0; i <= 1; i++) {
+            const posX = tile.x + dirX * i;
+            const posY = tile.y + dirY * i;
+
+            if (checkIsOutside((posX + 6), (posY + 6))) {
+                break;
+            }
+
+            neightboursTile.push(tiles[posX + 6][posY + 6]);
+        }
     }
 
-
-    return paths;
+    return neightboursTile;
 }
 
-console.log(`Path(tiles[5][-5], tiles[-5][5]); : ${ Path(tiles[5+6][-5+6], tiles[-5+6][5+6]) }`);
+console.log(`Path(tiles[5][-5], tiles[-5][5]); : ${Path(tiles[5 + 6][-5 + 6], tiles[-5 + 6][5 + 6])}`);
 
 export {Path}
--- Scripts/Tile.js
+++ Scripts/Tile.js
@@ -8,6 +8,10 @@ class Tile {
         this.block = block;
         this.upgrade = upgrade;
         this.bomb = null; //The variable checks whether a bomb is present => Upgrade Bomb / Dijkstra algorithme
+
+        this.G = 0;
+        this.H = 0;
+        this.F = 0;
     }
 }
 
